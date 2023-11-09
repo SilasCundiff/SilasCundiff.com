cd808ee3929cd62dbab317765e18686767e0e5d5
SilasCundiff <silinski23@gmail.com>
2023-11-09 14:19:01 -0500

    add basic layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,34 +1,46 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
-import Image from 'next/image'
-import logo from '../public/assets/logo.svg'
+import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
+import "./globals.css";
+import Image from "next/image";
 
-
-const inter = Inter({ subsets: ['latin'] })
+const poppinsFont = Poppins({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+});
 
 export const metadata: Metadata = {
-  title: 'Silas Cundiff',
-  description: 'A portfolio of my work.',
-}
+  title: "Silas Cundiff",
+  description: "A portfolio of my work.",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <header className="flex justify-center items-center p-4">
-          <nav className="flex items-center space-x-4">
-            <a className='font-light text-lg' href="#about">About</a>
-            <a className='px-8' href='#home'><Image alt="Silas Cundiff Logo" src="/assets/logo.svg" width="50" height="50" /></a>
-            <a className='font-light text-lg' href="#projects">Projects</a>
+      <body className={`${poppinsFont.className}`}>
+        <header className="flex justify-center items-center w-full p-4 fixed top-0 left-1/2 translate-x-[-50%] z-10">
+          <nav className="flex items-center space-x-4 text-slate-800">
+            <a className="font-light text-xl" href="#about">
+              About
+            </a>
+            <a className="px-8" href="#home">
+              <Image
+                alt="Silas Cundiff Logo"
+                src="/assets/logo.svg"
+                width="50"
+                height="50"
+              />
+            </a>
+            <a className="font-light text-xl" href="#projects">
+              Projects
+            </a>
           </nav>
         </header>
         {children}
-        </body>
+      </body>
     </html>
-  )
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,46 @@
+import Image from "next/image";
+import localFont from "next/font/local";
+
+const rockyBillyFont = localFont({
+  src: "./Rockybilly.ttf",
+});
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-     <h1>Silas Cundiff</h1>
+    <main className="flex min-h-screen flex-col items-center justify-between text-slate-800">
+      <section
+        id="home"
+        className="hero w-full h-screen relative flex justify-center items-center flex-col"
+      >
+        <h1
+          className={`${rockyBillyFont.className} font-semibold text-4xl mb-16`}
+        >
+          Silas Cundiff
+        </h1>
+        <h2 className="text-2xl font-normal">
+          Full Stack Developer & Designer
+        </h2>
+        {/* hero image background */}
+        <Image
+          className="absolute inset-0 w-full h-full object-cover z-[-1]"
+          alt="white paper background"
+          src="/assets/white-background.jpg"
+          width="500"
+          height="500"
+        />
+      </section>
+      <section
+        id="about"
+        className="about h-screen container bg-slate-100 mx-auto mt-24 p-8 rounded-t-lg"
+      >
+        <h2>About</h2>
+      </section>
+      <section
+        id="projects"
+        className="projects h-screen container bg-slate-100 mx-auto p-8 rounded-b-lg mb-24"
+      >
+        <h2>Projects</h2>
+      </section>
     </main>
-  )
+  );
 }
