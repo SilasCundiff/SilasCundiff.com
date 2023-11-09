aef1a7903820842177ac20f3c8db8040e1b80358
SilasCundiff <silinski23@gmail.com>
2023-11-09 13:19:34 -0500

    add basic nav

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import Image from 'next/image'
+import logo from '../public/assets/logo.svg'
+
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,7 +19,16 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <header className="flex justify-center items-center p-4">
+          <nav className="flex items-center space-x-4">
+            <a className='font-light text-lg' href="#about">About</a>
+            <a className='px-8' href='#home'><Image alt="Silas Cundiff Logo" src="/assets/logo.svg" width="50" height="50" /></a>
+            <a className='font-light text-lg' href="#projects">Projects</a>
+          </nav>
+        </header>
+        {children}
+        </body>
     </html>
   )
 }
